ec340c962bdba7c88d6a0afaddd556ec2898d445
Ryan <rayane.elmiloudi@esst-sup.com>
2026-02-23 01:32:42 +0100

    ryn

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rahim\Desktop\projects\TPSEC\venv
+set VIRTUAL_ENV=C:\Users\rayan\Documents\TPSECA\TPSEC\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
